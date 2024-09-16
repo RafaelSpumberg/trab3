081fe353cbb5f2d9484babe2cc82f99524cdb343
Rafael Spumberg <150760513+RafaelSpumberg@users.noreply.github.com>
2024-09-16 18:29:15 -0300

    Add files via upload

---
+++ endpointdemo1/src/main/java/com/bcopstein/endpointdemo1/Endpointdemo1Application.java
@@ -0,0 +1,13 @@
+package com.bcopstein.endpointdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Endpointdemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Endpointdemo1Application.class, args);
+	}
+
+}
